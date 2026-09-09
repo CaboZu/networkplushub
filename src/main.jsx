6962edf78f62f5bcbbfe12d8f61d768025fe1750
CaboZu <chris.cal.0987@gmail.com>
2026-09-09 17:12:03 -0500

    feat: mount Network+ chatbot alongside app

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.refreshed.jsx'
+import NetworkChatMount from './chat/NetworkChatMount.jsx'
 import { bootstrapProgressSync, installProgressAutoSave } from './progressCloud.js'
 
 async function start() {
@@ -11,6 +12,7 @@ async function start() {
   createRoot(document.getElementById('root')).render(
     <StrictMode>
       <App />
+      <NetworkChatMount />
     </StrictMode>,
   )
 }
